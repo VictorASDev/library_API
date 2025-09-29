857c85e9b4762458b97c4d49d5228312f0ac6a9e
Victor Augusto <victor.augustosilva004@gmail.com>
2025-09-28 21:51:09 -0300

    Update BookSummaryDTO.java

--- src/main/java/com/java/library/DTO/BookSummaryDTO.java
+++ src/main/java/com/java/library/DTO/BookSummaryDTO.java
@@ -9,7 +9,6 @@ public class BookSummaryDTO implements Serializable {
     private String publisher;
     private String synopsis;
 
-    // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
